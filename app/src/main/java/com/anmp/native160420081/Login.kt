8c7163126719bafb26b968c8ef35fef89ad8f147
Bayu <bayua9731@gmail.com>
2023-03-02 09:04:24 +0700

    init

---
+++ app/src/main/java/com/anmp/native160420081/Login.kt
@@ -0,0 +1,11 @@
+package com.anmp.native160420081
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
