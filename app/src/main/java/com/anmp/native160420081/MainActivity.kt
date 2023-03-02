12c6ea95c3b8028771663767266f4900f91605b5
Bayu <bayua9731@gmail.com>
2023-03-02 08:46:25 +0700

    init

---
+++ app/src/main/java/com/anmp/native160420081/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anmp.native160420081
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
